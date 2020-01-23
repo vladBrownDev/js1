b050776bf3c8ee22ee20bdd6268df027f933d587
vladik <vladen555555@gmail.com>
2020-01-23 15:31:17 +0300

    fixed

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 let inp = prompt("Введите 1 символ")
 
-console.log(inp, typeof(inp))
 
 if(inp == null) {
     document.write("Ввод отменен")
 }
 
-if(inp.length > 1 || inp.length == 0) {
-    let inp = " "
-}
-else {
-    let inpLower = inp.toLowerCase()
-    switch(inp) {
+let conf = inp.length > 1 || inp.length == 0 ? " " : inp.toLowerCase()
+
+
+    switch(conf) {
         case "a":
         case "e":
         case "o":
-            alert(inpLower.toUpperCase())
-        break
+            alert(conf.toUpperCase())
+            break
 
         case "x":
         case "y":
-            alert(inp + inp)
-        break
+            alert(conf + conf)
+            break
+
+
         case "s":
         case "q":
         case "d":
         case "m":
             let rand = Math.trunc(Math.random() * 10 + Math.random() * 10)
+            randResult = rand
             
             if(rand == 0) {
-                let rand = 1
+                randResult = 1
             }
 
-            alert(rand)
-        break
+            alert(randResult)
+            break
+
+
         case " ":
-            alert(Boolean(inp))
+            alert(Boolean(conf))
+            break
         default: 
             var rand1 = Math.trunc(Math.random() * 10)
             var rand2 = Math.trunc(Math.random() * 10)
             var rand3 = Math.trunc(Math.random() * 10)
             alert(Math.max(rand1, rand2, rand3))
     }
-}
