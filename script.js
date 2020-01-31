3670047af51b8ae9d28a973e2bfe0c9d9663e1b9
vladik <vladen555555@gmail.com>
2020-01-31 22:31:30 +0300

    asd

--- script.js
+++ script.js
@@ -1,81 +1,91 @@
-//1st task
+function myPow () {
+    num =  +prompt("Введите число")
+    pow = +prompt("Введите степень")
 
-let intString = ""
-
-outloop:for (i=0; i<=25; i++) {
-    let randInt = 65 + Math.round(Math.random() * (122 - 65))
-    intString = intString + String.fromCharCode(randInt)
-    if(randInt == "w" || randInt == "E") {
-        break outloop
+    if(!num || !pow) {
+        alert("Ошибка")
     }
-    
-}
-console.log(intString)
+    else {
+        numSt = num
+        do {
+            num*=numSt
+            pow-- 
+            
+        }
+        while (pow!==1) {
+            
+        }
 
-let intStringNumber = Math.round(Math.random() * (intString.length - 0))
-
-
-console.log(String(intString.charAt(intStringNumber)))
-
-if(intString.length < 5) {
-    for(i=0; i<=4; i++) {
-        console.log(intString)
+        alert(num)
     }
 }
-else {
-    console.log(intString.substring(5))
-}
+
+myPow()
 
 //2nd task
-
-let secResult = 12
-
-let num = 11
-
-let side2 = 1
-
-for(i=0, fact = 12; i!==12; i++) {
-
-    secResult = secResult * num
-    num--  
-    console.log(secResult)  
+let logArgsFunc = function() {
+    if(arguments.length !== 0) {
+       for(let i = 0; i < arguments.length; i++) {
+           
+           if(typeof arguments[i] == "function") {
+               arguments[i]()
+               document.write(`Function was called <br>`)
+           }
+           else{
+                document.write(`${arguments[i]}, type: ${typeof(arguments[i])} <br>`)  
+           }
+            
+        } 
+    }
+    else {
+        document.write("No arguments in function <br>")
+    }
     
 }
 
-
-//3rd task
-
-for(i=1; i<=4; i++) {
-    console.log("#".repeat(i))
+function uselessFunction () {
+    console.log('I am Function')
 }
 
-//4th task
+logArgsFunc ()
 
-let score = 4
+logArgsFunc (100)
 
-let testStr = "    "
+logArgsFunc (100, "test", uselessFunction)
 
-while(score!==0) {
 
-    switch(score) {
-        case 4:
-            newStr = testStr.replace("    ", "   #");
-            console.log(newStr)
-            break;
-        case 3:
-            newStr = testStr.replace("    ", "  ##");
-            console.log(newStr)
-            break;
-        case 2:
-            newStr = testStr.replace("    ", " ###");
-            console.log(newStr)
-            break;
-        case 1:
-            newStr = testStr.replace("    ", "####");
-            console.log(newStr)
-            break;
-        default :
-            break;
+
+
+
+
+
+//3nd task
+
+let palStr = prompt("проверка слова на симетричность")
+
+let check = palStr.length - 1
+
+function pal(Str) {
+    let fstSymb = 0
+    let secSymb = Str.length - 1
+
+    for(let i=0;i!==Str.length - 1;i++) {
+
+        if(Str[fstSymb] == Str[secSymb]) {
+            check = true
+            fstSymb++
+            secSymb--
+        
+        }
+
+        else {
+            check = false
+        }
+
+        
     }
-    score--
+    return check
+
+
 }
+alert(pal(palStr))
