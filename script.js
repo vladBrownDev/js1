0c804f53230cd69b4466d632eb881c5efb12255d
vladik <vladen555555@gmail.com>
2020-02-12 22:22:42 +0300

    done

--- script.js
+++ script.js
@@ -1,122 +1,108 @@
-var randArray = []
-const randomIntArea = 30 + Math.random() * (50 - 30)
-for (let i=randomIntArea; i>0; i-- ) {
-    randArray.push(Math.floor(-100 + Math.random() * (100 + 100)))
+"use strict"
+let testObj = {}
+testObj["prop"] = "test property"
+console.log("log prop   ",testObj["prop"])
+console.log("log prop2   ",testObj.prop)
+
+
+
+testObj["pi"] = 3.14
+delete testObj.prop
+console.log("deleted prop   ",testObj)
+
+testObj["sumOfTwo"] = function sumOfTwoFunc (x,y) {
+    return x + y
+}
+console.log("type sumOfTwo   ",typeof testObj.sumOfTwo("40", 150))
+
+let newObj = Object.assign({"newProp":{"x":10}}, testObj)
+console.log("newObj   ",newObj)
+
+for (let key in testObj) {
+    delete testObj[key]
+}
+console.log("delete   ",testObj)
+
+let copiedObj = {...newObj}
+console.log("copied   ",copiedObj)
+
+copiedObj["newProp"] = {x:100}
+console.log("x eq 100   ",copiedObj)
+
+console.log("newprop in copiedObj and newObj   ",copiedObj["newProp"], newObj["newProp"])
+
+let arrayForCope = []
+
+for (let key in copiedObj) {
+    arrayForCope.push(`${key}, ${copiedObj[key]}`)
+}
+console.log("obj to arr   ",arrayForCope)
+
+function constructObject(objectName = "noName", objectNumber = "0", objectString="") {
+    let obj = {}
+    obj.Name = objectName
+    obj.String = objectString
+    obj.Number = objectNumber
+    return obj
 }
 
-console.log(randArray)
-
-// for (i=randArray.length - 1 ; i>=0; i--) {
-//     randArray[i] = randArray[i] + 2
-// }
+let createdObj1 = constructObject("TEST","10", "im string")
+let createdObj2 = constructObject()
+console.log(createdObj2, createdObj1)
 
 
-const plusTwoArray = []
-
-randArray.forEach(function(element){
-    plusTwoArray.push(element + 2)
-})
-
-
-console.log("plus two",plusTwoArray)
+let createdArr1 = []
+let createdArr2 = []
+createdArr1.push(Object.keys(createdObj1))
+createdArr2.push(Object.keys(createdObj2))
+console.log(createdArr1, createdArr2)
 
 
+function deepClone(obj) {
+    let copyObj = {}
+    for(let key in obj) {
+        if(Array.isArray(obj[key])) {
+            copyObj[key] = [...obj[key]]
+            console.log("Array.isArray")
 
-let mid = Math.floor(randArray.length/2)
-let midVal = randArray[mid]
-console.log(midVal)
-
-
-
-delete randArray[0]
-randArray[0] = randArray[mid]
-console.log(randArray)
-
-
-
-
-const biggerThanZero = (item, index, array) => {
-    if(item<0) {
-        return true
+        }
+        
+        
+        else {
+            if(typeof obj[key] === "object") {
+                copyObj[key] = deepClone(obj[key])
+                
+            }else {
+                copyObj[key] = obj[key]
+            }
+            
+        }
     }
-    else {
-        return false
-    }
+    return copyObj
+        
+		
+		
+	
 }
 
-console.log("biggerThanZero",randArray.every(biggerThanZero))
-
-
-
-const equalZero = (item, index, array) => {
-    if(item === 0) {
-        return true
-    }
-    else {
-        return false
-    }
+const targetObj = {
+    x: 1,
+    y: {
+        w: 'test',
+        q: {
+            a: true,
+            b: () => console.log('useless'),
+            c: {
+                d: ['a', 'b', 'c']
+            }
+        }
+    },
+    z: [1,2,3,4,5]
 }
+let copied = deepClone(targetObj)
+console.log(copied)
+console.log(targetObj)
 
-console.log("eqal zero", randArray.some(equalZero))
-
-
-
-let string = 'Lorem ipsum dolor sit amet'
-let reversed = string.split('').reverse().join('')
-console.log(reversed)
-
-
-
-let secString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-let splitSecString = secString.split(". ")
-console.log(splitSecString)
-
-
-
-splitSecString[splitSecString.length - 1] = (splitSecString[splitSecString.length - 1]).toUpperCase()
-console.log(splitSecString)
-
-
-
-let newString = splitSecString.join("\n")
-console.log(newString)
-
-
-let firstArray = []
-for(let i=0; i<10; i++) {
-    firstArray[i] = (Math.floor(Math.random() * 10)) 
-}
-console.log(firstArray)
-
-let secondArray = []
-for(let i=0; i<10; i++) {
-    secondArray[i] = (-10 + Math.floor(Math.random() * 10)) 
-}
-console.log(secondArray)
-
-let unitedArray = firstArray.concat(secondArray)
-console.log(unitedArray)
-
-function funcMap (element,index,array) {
-    return element / 5
-}
-
-let divisedArray = unitedArray.map(funcMap)
-console.log(divisedArray)
-
-function sortFunc (next, prev) {
-    return prev - next
-}
-divisedArray.sort(sortFunc)
-console.log(divisedArray)
-
-function filtFunc(num) {
-    return (num ^ 0) === num
-
-}
-
-let filteredArray = divisedArray.filter(filtFunc)
-console.log(filteredArray)
-
-filteredArray.splice(0,filteredArray.length);
-console.log(filteredArray)
+targetObj.y.q.c.d[0] = 100
+console.log(copied)
+console.log(targetObj)
